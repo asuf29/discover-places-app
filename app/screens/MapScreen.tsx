bf7b0401a3852254cb2176da72b090340acc75b7
asuf29 <asuf29@icloud.com>
2024-08-27 22:20:13 +0300

    The user can directly see her location when the map is opened.

--- app/screens/MapScreen.tsx
+++ app/screens/MapScreen.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
-function MapScreen() {
+const MapScreen = () => {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    };
+
+    getLocation();
+  }, []);
+
   return (
-    <View>
-      <Text>Map Screen</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} region={mapRegion} />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default MapScreen;
