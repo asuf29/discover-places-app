cab1999ff1e0ec46a5e9f3d8aa39201edaad9f87
asuf29 <asuf29@icloud.com>
2024-08-27 14:54:32 +0300

    add top trips

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -5,10 +5,11 @@ import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SearchBar from './SearchBar';
 import CategoryList from '../components/CategoryList';
+import TopTrips from '../components/TopTrips';
 
 function HomeScreen() {
   return (
-    <SafeAreaView style={tw`flex-1 mt-3`}>
+    <SafeAreaView style={tw`flex-1`}>
       <ScrollView contentContainerStyle={tw`items-center px-4`}>
         <View style={tw`flex-row items-center justify-between w-full`}>
           <View style={tw`flex-row items-center`}>
@@ -20,21 +21,19 @@ function HomeScreen() {
           </View>
           <Ionicons name="notifications" size={24} color="black" />
         </View>
-
         <View style={tw`w-full mt-5 p-4 rounded-lg`}>
           <Text style={tw`text-2xl font-bold`}>
             Where do{'\n'}you want to go?
           </Text>
           <SearchBar />
         </View>
-
         <View style={tw`w-full mt-5 p-4 rounded-lg`}>
           <Text style={tw`text-base font-bold mb-2`}>Categories</Text>
           <CategoryList />
         </View>
-
         <View style={tw`w-full mt-5 p-4 rounded-lg mb-20`}>
-          <Text style={tw`text-base font-bold`}>Top trips</Text>
+          <Text style={tw`text-base font-bold `}>Top trips</Text>
+          <TopTrips />
         </View>
       </ScrollView>
     </SafeAreaView>
