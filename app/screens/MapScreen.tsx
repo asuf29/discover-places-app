c1121227af29577769458a80e3fb14a124753aef
asuf29 <asuf29@icloud.com>
2024-08-27 22:40:52 +0300

    search bar added to map

--- app/screens/MapScreen.tsx
+++ app/screens/MapScreen.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
+import tw from 'twrnc';
 
 const MapScreen = () => {
+  const [searchQuery, setSearchQuery] = useState('');
   const [mapRegion, setMapRegion] = useState({
     latitude: 37.78825,
     longitude: -122.4324,
@@ -34,6 +37,21 @@ const MapScreen = () => {
   return (
     <View style={styles.container}>
       <MapView style={styles.map} region={mapRegion} />
+      <View
+        style={tw`absolute top-12 left-8 right-8 flex-row items-center p-2 rounded-md shadow-md h-10 bg-transparent border border-gray-200`}
+      >
+        <TouchableOpacity style={tw``}>
+          <FontAwesome name="search" size={16} color="white" />
+        </TouchableOpacity>
+        <TextInput
+          placeholder="Search for places..."
+          style={tw`flex-1 text-stone-950 ml-2`}
+          value={searchQuery}
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={(text) => setSearchQuery(text)}
+        />
+      </View>
     </View>
   );
 };
@@ -45,6 +63,18 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  searchContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    right: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    elevation: 2,
   },
 });
 
