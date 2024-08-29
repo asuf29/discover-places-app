1017ed38d5f8d8bf79bed71562642121ef29986e
asuf29 <asuf29@icloud.com>
2024-08-29 13:32:32 +0300

    back page redirected

---
+++ app/components/BackPageRedirect.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import tw from 'twrnc';
+import { useNavigation } from '@react-navigation/native';
+import MapScreen from '../screens/MapScreen';
+
+const BackPageRedirect = () => {
+  const navigation = useNavigation();
+  const handleBack = () => {
+    navigation.navigate('Map');
+  };
+
+  return (
+    <View
+      style={tw`absolute w-full h-18 top-0 flex-row items-center justify-start px-4 pt-4 border-b border-gray-300 bg-gray-800 z-10`}
+    >
+      <TouchableOpacity
+        onPress={handleBack}
+        style={tw`flex-row p-2 rounded-md mx-2`}
+      >
+        <FontAwesome name="arrow-left" size={16} color="white" />
+        <Text style={tw`text-white text-center font-bold ml-2`}>
+          Back to map!
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BackPageRedirect;
