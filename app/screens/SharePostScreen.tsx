cebbd59a07e56aa6fd5a9b1003dfc4fc61a0fef6
asuf29 <asuf29@icloud.com>
2024-09-08 23:00:46 +0300

    top menu and photo import completed

--- app/screens/SharePostScreen.tsx
+++ app/screens/SharePostScreen.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Ionicons } from '@expo/vector-icons';
 import tw from 'twrnc';
+import { NavigationProp } from '@react-navigation/native';
 
-function SharePostScreen() {
+function SharePostScreen({ navigation }: { navigation: NavigationProp<any> }) {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (result.assets && result.assets.length > 0) {
+      setImage(result.assets[0].uri);
+      console.log('Selected Image URI:', result.assets[0].uri);
+    }
+  };
+
+  const handlePost = () => {
+    console.log('Image:', image);
+  };
+
   return (
-    <View style={tw`flex-1 items-center justify-center`}>
-      <Text style={tw`text-xl font-semibold`}>Share Post Screen</Text>
+    <View style={tw`flex-1 bg-white`}>
+      <View
+        style={tw`flex-row justify-between items-center p-4 border-b border-gray-200 mt-4`}
+      >
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={tw`text-lg font-semibold ml-8`}>New Post</Text>
+        <Button title="Share" onPress={handlePost} color="#3498db" />
+      </View>
+
+      <ScrollView style={tw`flex-1 p-4 ml-4`}>
+        <View style={tw`items-center`}>
+          {image ? (
+            <TouchableOpacity onPress={pickImage}>
+              <Image source={{ uri: image }} style={tw`w-50 h-50 rounded-lg`} />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              onPress={pickImage}
+              style={tw`w-50 h-50 bg-gray-300 rounded-lg justify-center items-center`}
+            >
+              <Text style={tw`text-gray-500`}>Add Image</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 }
